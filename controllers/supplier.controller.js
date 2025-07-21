3560c9d54a1497c45155839c4efd11b20195d535
arshansari-c <arsh3434y@gmail.com>
2025-07-21 14:28:24 +0530

    Update supplier.controller.js

--- controllers/supplier.controller.js
+++ controllers/supplier.controller.js
@@ -3,7 +3,7 @@ import {SupplierList} from '../models/supplier.model.js';
 
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
-
+import jwt from 'jsonwebtoken'
 cloudinary.config({
   cloud_name: process.env.CLOUDNARY_NAME,
   api_key: process.env.CLOUDNARY_API,
