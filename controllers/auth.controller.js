57bdb1c0fe9f813ce7b4a41b84a14ccc63b17bce
arshansari-c <arsh3434y@gmail.com>
2025-08-03 13:58:01 +0530

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,62 @@
 
 import { User } from '../models/auth.model.js'; // adjust the path if needed
 import bcrypt from 'bcryptjs';
+import { v2 as cloudinary } from 'cloudinary';
 import jwt from 'jsonwebtoken';
+cloudinary.config({
+  cloud_name: process.env.CLOUDNARY_NAME,
+  api_key: process.env.CLOUDNARY_API,
+  api_secret: process.env.CLOUDNARY_SECRET,
+});
+
+export const UpdateProfile = async (req, res) => {
+  try {
+    // Extract token from header
+    const {token} = req.params // e.g., Bearer <token>
+
+    if (!token) {
+      return res.status(401).json({ message: "Token missing" });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
+    if (!decoded?.userId) {
+      return res.status(400).json({ message: "Invalid token" });
+    }
+
+    const findUser = await User.findById(decoded.userId).select("-password");
+    if (!findUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const { username, password } = req.body;
+
+    // Update username if provided
+    if (username) {
+      findUser.username = username;
+    }
+
+    // Update password securely
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      findUser.password = hashedPassword;
+    }
+
+    // Update profile photo if provided
+    if (req.files?.photo) {
+      const photo = req.files.photo;
+      const uploadPhoto = await cloudinary.uploader.upload(photo.tempFilePath);
+      findUser.image = uploadPhoto.secure_url;
+    }
+
+    await findUser.save();
+    return res.status(200).json({ message: "Profile updated successfully" });
+
+  } catch (error) {
+    console.log("❌ UpdateProfile error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -53,7 +108,6 @@ export const register = async (req, res) => {
     return res.status(201).json({
       message: "User registered successfully",
       userId: newUser._id,
-      role : newUser.role,
       token, // <--- Mobile app will save this in AsyncStorage
     });
 
