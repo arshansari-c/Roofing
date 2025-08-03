95645254014653ff2f962adaf59cf5629bc85232
arshansari-c <arsh3434y@gmail.com>
2025-08-03 19:27:45 +0530

    Update auth.model.js

--- models/auth.model.js
+++ models/auth.model.js
@@ -6,7 +6,6 @@ const AuthSchema = new Schema(
     username: {
       type: String,
       required: [true, "Username is required"],
-      unique: true,
       trim: true,
       minlength: [3, "Username must be at least 3 characters"],
       maxlength: [30, "Username must be less than 30 characters"],
