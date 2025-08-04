a4cc36164cfaaa4faa2cfe295e8eef12e4a67b4a
arshansari-c <arsh3434y@gmail.com>
2025-08-04 13:13:59 +0530

    Update auth.route.js

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addTeamMemberEmail, login,  Profile, register, sendPdfToTeamFromEmail, UpdateProfile } from '../controllers/auth.controller.js'
+import { addTeamMemberEmail, fetchTeamEmails, login,  Profile, register, sendPdfToTeamFromEmail, UpdateProfile } from '../controllers/auth.controller.js'
 import { CheckAuth } from '../middlewares/CheckAuth.js'
 import { EditSupplierDetails, fetchSupplierList, SupplierDetails } from '../controllers/supplier.controller.js'
 import { EditFreelancerDetails, fetchFreelancerList, fetchOtherFreelancerDetails, fetchOtherUserDetails, FreelancerDetails } from '../controllers/freelancer.controller.js'
@@ -35,3 +35,4 @@ AuthRouter.get('/fetchuserpdf/:userId',fetchUserPdf)
 AuthRouter.put('/changepassword/:userId',ChangePassword)
 AuthRouter.post('/savedEmails/:userId',addTeamMemberEmail)
 AuthRouter.post('/sendpdftoteam/:userId',sendPdfToTeamFromEmail)
+AuthRouter.post('/fetchteamemail/:userId',fetchTeamEmails)
