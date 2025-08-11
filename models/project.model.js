4d9b89b5459bf7745ad7fcfdee551747bc032def
arshansari-c <arsh3434y@gmail.com>
2025-08-11 13:37:43 +0530

    Update project.model.js

--- models/project.model.js
+++ models/project.model.js
@@ -4,7 +4,16 @@ import mongoose, { Schema } from "mongoose";
 const ProjectSchema = new Schema(
   {
     userId: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
-    data: { type: Map, of: Schema.Types.Mixed, required: true },
+    pdf : [{
+      public_id : {
+        type : String,
+        required : true
+      },
+      url:{
+        type : String,
+        required : true
+      },
+    }],
     Name: { type: String, required: true },
     Code: { type: String, required: true },
     Color: { type: String, required: true },
